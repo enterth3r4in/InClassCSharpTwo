39b8404030229dbe5ac066159abb7301ff32397f
enterth3r4in <believe.the.rain@gmail.com>
2016-01-25 14:00:28 -0500

    Progress update

--- InClassDemoTwo/InClassDemoTwo/Form1.Designer.cs
+++ InClassDemoTwo/InClassDemoTwo/Form1.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shiny New Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
